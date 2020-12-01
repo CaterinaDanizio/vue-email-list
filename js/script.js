f23c4181a00cf8d5711319ef010c48103ae43932
Caterina <danizio@outlook.it>
2020-12-01 21:42:32 +0100

    Fix

--- js/script.js
+++ js/script.js
@@ -8,27 +8,27 @@ new Vue({
   },
 
 // Generazione email con mounted function
-  mounted: function () {
-    for( var i=0; i<10; i++)
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(emailRand => {
-       let email = emailRand.data.response;
-       if (!this.mailingList.includes(email))
-         this.mailingList.push(email);
-     });
-    console.log(this.mailingList);
-  },
+  // mounted: function () {
+  //   for( var i=0; i<10; i++)
+  //   axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+  //   .then(emailRand => {
+  //      let email = emailRand.data.response;
+  //      if (!this.mailingList.includes(email))
+  //        this.mailingList.push(email);
+  //    });
+  //   console.log(this.mailingList);
+  // },
 
 // Generazione email tramite bottone
-  // methods: {
-  //   emailGen: function () {
-  //    for (var i = 0; i < 10; i++) {
-  //    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(emailRand => {
-  //     let email = emailRand.data.response;
-  //     if (!this.mailingList.includes(email)) this.mailingList.push(email)
-  //     });
-  //     }
-  //     console.log(this.mailingList);
-  //     }
-  //     },
+  methods: {
+    emailGen: function () {
+     for (var i = 0; i < 10; i++) {
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(emailRand => {
+      let email = emailRand.data.response;
+      if (!this.mailingList.includes(email)) this.mailingList.push(email)
+      });
+      }
+      console.log(this.mailingList);
+      }
+      },
 });
